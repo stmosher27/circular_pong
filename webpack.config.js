a8668fd43d946726504ead0fbb6e41b6cdb3cc2d
stmosher27 <stmosher27@gmail.com>
2017-01-24 15:03:16 -0800

    Finish collision

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './lib/entry.js',
+  output: {
+    path: './lib',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx' ]
+  }
+};
